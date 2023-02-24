1e1007069cc1a94dd3c7f0ef2a34521eb68815b3
Shrey <shreyraj1653@gmail.com>
2023-02-24 22:19:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express') ; 
+const app = express() ; 
+const hbs = require('hbs') ; 
+// const expressLayout = require('express-ejs-layouts') ; 
+const bodyParser = require('body-parser') ; 
+
+// app.use(expressLayout) ; 
+
+// Use the body-parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Set template Engines
+app.set('views' , 'resources/views' ) ;
+app.set('view engine' , 'hbs') ;  
+
+const PORT = process.env.PORT || 3005 ;
+
+app.get('/' ,  (req,res)=>{
+    res.render('home') ; 
+}); 
+
+app.listen(PORT , ()=>{console.log(`Listening on Port ${PORT}`)}) ; 
+  
